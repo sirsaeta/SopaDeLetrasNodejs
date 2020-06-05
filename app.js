4c041b3487a2d3ff11e39d09213fe79266b2b528
Axel Martinez <sir_saeta@hotmail.com>
2020-06-05 14:09:35 -0300

    Quitar variables no utilizadas

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
+//lectura sopa de letras
+// http://trackmaze.blogspot.com/2013/01/resolver-una-sopa-de-letras-con-python.html
 const hostname = '127.0.0.1';
 const port = 3001;
 const express = require('express')
 const app = express()
 const palabraBuscar = "OIE";
-const allOrientations = ['horizontal','horizontalBack','vertical','verticalUp',
-						'diagonal','diagonalUp','diagonalBack','diagonalUpBack'];
-
-var orientations = {
-	horizontal:     function(x,y,i) { return {x: x+i, y: y  }; },
-	horizontalBack: function(x,y,i) { return {x: x-i, y: y  }; },
-	vertical:       function(x,y,i) { return {x: x,   y: y+i}; },
-	verticalUp:     function(x,y,i) { return {x: x,   y: y-i}; },
-	diagonal:       function(x,y,i) { return {x: x+i, y: y+i}; },
-	diagonalBack:   function(x,y,i) { return {x: x-i, y: y+i}; },
-	diagonalUp:     function(x,y,i) { return {x: x+i, y: y-i}; },
-	diagonalUpBack: function(x,y,i) { return {x: x-i, y: y-i}; }
-};
-var checkOrientations = {
-	horizontal:     function(x,y,h,w,l) { return w >= x + l; },
-	horizontalBack: function(x,y,h,w,l) { return x + 1 >= l; },
-	vertical:       function(x,y,h,w,l) { return h >= y + l; },
-	verticalUp:     function(x,y,h,w,l) { return y + 1 >= l; },
-	diagonal:       function(x,y,h,w,l) { return (w >= x + l) && (h >= y + l); },
-	diagonalBack:   function(x,y,h,w,l) { return (x + 1 >= l) && (h >= y + l); },
-	diagonalUp:     function(x,y,h,w,l) { return (w >= x + l) && (y + 1 >= l); },
-	diagonalUpBack: function(x,y,h,w,l) { return (x + 1 >= l) && (y + 1 >= l); }
-};
-var skipOrientations = {
-	horizontal:     function(x,y,l) { return {x: 0,   y: y+1  }; },
-	horizontalBack: function(x,y,l) { return {x: l-1, y: y    }; },
-	vertical:       function(x,y,l) { return {x: 0,   y: y+100}; },
-	verticalUp:     function(x,y,l) { return {x: 0,   y: l-1  }; },
-	diagonal:       function(x,y,l) { return {x: 0,   y: y+1  }; },
-	diagonalBack:   function(x,y,l) { return {x: l-1, y: x>=l-1?y+1:y    }; },
-	diagonalUp:     function(x,y,l) { return {x: 0,   y: y<l-1?l-1:y+1  }; },
-	diagonalUpBack: function(x,y,l) { return {x: l-1, y: x>=l-1?y+1:y  }; }
-};
 
 // Create HTTP error
 
